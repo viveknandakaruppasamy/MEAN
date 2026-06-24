281961c1050f7f66dd587f432221057ff3583121
VIVEK NANDA K <145146077+viveknandakaruppasamy@users.noreply.github.com>
2026-06-24 13:08:37 +0530

    mongodb changes is get saved

--- node-practice/server1.js
+++ node-practice/server1.js
@@ -1,97 +1,136 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Employee = require('./models/Employee');
+
 const app = express();
+
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/employeedb'
+)
+.then(() => {
+    console.log('MongoB  Connected');
+})
+.catch((err) => {
+    console.log(err);
+});
 
 
 app.use(cors());
 app.use(express.json());
 
-let employees = [];
+app.post('/employees', async ( req, res) => {
 
-app.post('/employees', ( req, res) => {
+    try {
 
-    const employee =  {
-        id : employees.length + 1,
+    const employee = new Employee ({
         Name : req.body.Name,
         Email : req.body.Email,
         phone : req.body.phone,
         Department : req.body.Department,
         designation : req.body.designation,
         salary : req.body.salary,
-    };
+    });
 
-    employees.push(employee);
+    await employee.save();
 
     res.json({
         message: 'User Created Successfully',
         employee
-        
     });
+} catch(error) {
+
+    res.status(500).json({
+        message: error.message
+    });
+
+}
+
 });
 
-app.get('/employees', (req, res) => {
-    res.json(employees);
-});
+app.get('/employees', async (req, res) => {
 
-
-    app.put('/employees/:id', (req, res) => {
-
-        console.log('ID:', req.params.id);
-        console.log('BODY:', req.body);
+    try {
         
-        const employee = employees.find(
-        employees => employees.id === Number(req.params.id)
-        );
+        const employees = await Employee.find();
+        
+        res.json(employees);
+    }
+    catch(error) {
 
-        console.log('FOUND USER:', employee);
-
-
-        if(!employee)
-        {
-            return res.json({
-            message: "User not found"
+        res.status(500).json({
+            message: error.message
         });
+    }
+});
+
+
+    app.put('/employees/:id', async  (req, res) => {
+
+        try {
+
+            const employee = await Employee.findByIdAndUpdate(
+
+                req.params.id,
+
+                {
+                    Name : req.body.Name,
+                    Email : req.body.Email,
+                    phone : req.body.phone,
+                    Department : req.body.Department,
+                    designation : req.body.designation,
+                    salary : req.body.salary
+
+                },
+
+                { new : true }
+
+            );
+
+            if(!employee){
+
+                return res.json({
+                    message : 'User not found'
+                });
+
+            }
+
+            res.json({
+                message : 'User updated successfully',
+
+                employee
+            });
+
+        } catch(error){
+
+            res.status(500).json({
+                message: error.message
+            });
         }
 
-
-    employee.Name = req.body.Name;
-
-    employee.phone = req.body.phone;
-
-    employee.Email = req.body.Email;
-
-    employee.Department = req.body.Department;
-
-    employee.designation = req.body.designation;
-
-    employee.salary = req.body.salary;
-
-    console.log('UPDATED USER:', employee);
-
-    res.json({
-        message: 'User updated successfully',
-        employee : employee
-    });
-
 });
 
-app.delete('/employees/:id', (req, res) => {
+app.delete('/employees/:id', async  (req, res) => {
 
-        console.log('Id from URL:', req.params.id);
-        
+    try {
 
-    employees = employees.filter(
-        employee => employee.id !== Number(req.params.id)
+        await Employee.findByIdAndDelete(
+            req.params.id
         );
 
+        res.json({
+            message : 'User deleted Successfully'
+        });
 
-    console.log('Employees after delete:', employees);
+    } catch(error){
 
-    res.json({
-        message: 'User deleted Successfully'
+        res.status(500).json({
+            message : error.message
     });
 
-    });
+}
+
+});
 
 app.listen(3000, () => {
     console.log("The server running on port 3000");
